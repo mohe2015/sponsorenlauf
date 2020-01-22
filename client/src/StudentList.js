5cc59a72e7ca37f55c6454de70ce46071dd978c2
dev.mohe <Moritz.Hedtke@t-online.de>
2020-01-22 21:22:33 +0100

    Split by class

--- client/src/StudentList.js
+++ client/src/StudentList.js
@@ -6,23 +6,41 @@ import Student from './Student'
 import Table from 'react-bootstrap/Table'
 
 export default class StudentList extends React.Component {
+  
+  groupBy = function(xs, key) {
+    return xs.reduce(function(rv, x) {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+  };
+
+  values = (dictionary) => {
+    return Object.keys(dictionary).map(function(key){
+      return dictionary[key];
+    });
+  }
+
   render() {
     return (
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Startnummer</th>
-            <th>Name</th>
-            <th>Klasse</th>
-            <th>Jahrgang</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.viewer.map((student) =>
-              <Student key={student.__id} student={student} />
-          )}
-        </tbody>
-      </Table>
+      <div>
+      {this.values(this.groupBy(this.props.viewer, 'class')).map(clazz =>
+        <Table striped bordered hover style={{pageBreakInside: 'avoid'}}>
+          <thead>
+            <tr>
+              <th>Startnummer</th>
+              <th>Name</th>
+              <th>Klasse</th>
+              <th>Jahrgang</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clazz.map((student) =>
+                <Student key={student.__id} student={student} />
+            )}
+          </tbody>
+        </Table>
+      )}
+      </div>
     );
   }
 }
