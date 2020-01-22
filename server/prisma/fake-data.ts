b3775521962171ab72b83e04a5de7f059ed5fb3a
dev.mohe <Moritz.Hedtke@t-online.de>
2020-01-22 18:59:53 +0100

    Import data

--- server/prisma/fake-data.ts
+++ server/prisma/fake-data.ts
@@ -1,10 +1,14 @@
 import { Photon } from '@prisma/photon'
 import { hash } from 'bcrypt'
+const csv = require('csv-parser');
+const fs = require('fs');
 
 main()
 
 async function main() {
   const photon = new Photon()
+  
+  /*
   const hashedPassword = await hash('admin', 10)
   const admin = await photon.users.create({
     data: {
@@ -14,5 +18,25 @@ async function main() {
     },
   })
   console.log('added admin account:\n', admin)
+  */
+
+ fs
+ .createReadStream('prisma/test-data.csv')
+ .pipe(csv())
+ .on('data', (data:any) => { 
+   console.log(data)  
+   photon.students.create({
+    // @ts-ignore
+     data: {
+       name: data['Name'],
+       class: data['Klasse'],
+       grade: data['Jahrgang'],
+     }
+   })
+  })
+ .on('end', () => {
+   console.log("end");
+ });
+
   await photon.disconnect()
 }
