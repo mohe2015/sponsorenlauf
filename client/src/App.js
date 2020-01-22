d146b375adda465c9a9eb340f9ade46dce46631a
dev.mohe <Moritz.Hedtke@t-online.de>
2020-01-22 20:28:38 +0100

    Clean login and logout

--- client/src/App.js
+++ client/src/App.js
@@ -49,19 +49,19 @@ class App extends Component {
           
  
 
-          <Dropdown className="order-md-4" alignRight>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                  <svg class="bi bi-person" width="2em" height="2em" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M15 16s1 0 1-1-1-4-6-4-6 3-6 4 1 1 1 1h10zm-9.995-.944v-.002zM5.022 15h9.956a.274.274 0 00.014-.002l.008-.002c-.001-.246-.154-.986-.832-1.664C13.516 12.68 12.289 12 10 12c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664a1.05 1.05 0 00.022.004zm9.974.056v-.002zM10 9a2 2 0 100-4 2 2 0 000 4zm3-2a3 3 0 11-6 0 3 3 0 016 0z" clip-rule="evenodd"/>
-                  </svg>   
-                </Dropdown.Toggle>
+          {this.state.userId &&
+            <Dropdown className="order-md-4" alignRight>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                <svg class="bi bi-person" width="2em" height="2em" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" d="M15 16s1 0 1-1-1-4-6-4-6 3-6 4 1 1 1 1h10zm-9.995-.944v-.002zM5.022 15h9.956a.274.274 0 00.014-.002l.008-.002c-.001-.246-.154-.986-.832-1.664C13.516 12.68 12.289 12 10 12c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664a1.05 1.05 0 00.022.004zm9.974.056v-.002zM10 9a2 2 0 100-4 2 2 0 000 4zm3-2a3 3 0 11-6 0 3 3 0 016 0z" clip-rule="evenodd"/>
+                </svg>   
+              </Dropdown.Toggle>
 
-                <Dropdown.Menu>
-                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                  <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
+              <Dropdown.Menu>
+                <Dropdown.Item href='/' onClick={this.logout}>Abmelden</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          }
           
           <Navbar.Toggle className="order-md-2" aria-controls="responsive-navbar-nav" />
         
@@ -72,18 +72,11 @@ class App extends Component {
               <Nav.Link href="/students">Schüler</Nav.Link>
             </Nav>
             <Nav>
-              
+              {(!this.state.userId) && <Nav.Link href="/login">Anmelden</Nav.Link>}
             </Nav>
           </Navbar.Collapse>
 
-
-
         </Navbar>
-        
-        {this.state.userId ?
-          <Link to='/' onClick={this.logout}>Logout</Link>
-          :
-          <Link to='/login'>Login</Link>}
 
         <Switch>
           <Route exact path="/">
