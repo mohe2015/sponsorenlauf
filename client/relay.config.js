9aa117248499d5dee634d71f733a0f0ece43a10d
dev.mohe <Moritz.Hedtke@t-online.de>
2020-01-27 15:53:59 +0100

    Formatting

--- client/relay.config.js
+++ client/relay.config.js
@@ -3,6 +3,5 @@ module.exports = {
   // Configuration options accepted by the `relay-compiler` command-line tool and `babel-plugin-relay`.
   src: "./src",
   schema: "../server/generated/schema.graphql",
-  exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
-}
-
+  exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"]
+};
