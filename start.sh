b7c62003161f84f3c4c4e4b8101c590230800651
dev.mohe <Moritz.Hedtke@t-online.de>
2020-01-28 18:07:23 +0100

    Small fix

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 tmux new-session -s sponsorenlauf -d 'cd client; yarn run relay --watch'
-tmux split-window -v -t 0 'cd server; yarn prisma2 dev'
+tmux split-window -v -t 0 'cd server; yarn prisma2 studio --experimental'
 tmux split-window -v -t 0 'cd client; yarn run start'
 tmux split-window -v -t 2 'cd server; yarn run dev'
 tmux attach-session -t sponsorenlauf
