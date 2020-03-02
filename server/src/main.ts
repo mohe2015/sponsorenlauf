34c21fd22742abab490d94dfef8b81435a56e92d
dev.mohe <Moritz.Hedtke@t-online.de>
2020-03-02 19:15:20 +0100

    Switch to apollo server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,36 +1,42 @@
-import { GraphQLServer } from 'graphql-yoga'
+import { ApolloServer, makeExecutableSchema } from 'apollo-server'
 import schema from './schema'
 import { createContext, Context } from './context'
-import { permissions } from './permissions'
 import { verify } from 'jsonwebtoken'
 import { Token, APP_SECRET } from './utils'
+import { WebSocket } from 'ws'
+import { ConnectionContext } from 'subscriptions-transport-ws'
 
-const server = new GraphQLServer({
+const server = new ApolloServer({
   schema,
   context: createContext,
-  middlewares: [permissions],
+  playground: process.env.NODE_ENV === 'development',
+  debug: process.env.NODE_ENV === 'development',
+  subscriptions: {
+    onConnect: (
+      connectionParams: Object,
+      websocket: WebSocket,
+      context: ConnectionContext,
+    ) => {
+      //console.log('onConnect')
+      let Authorization
+      if (connectionParams.Authorization) {
+        //console.log('websocket', connectionParams.Authorization)
+        Authorization = connectionParams.Authorization
+      }
+      if (Authorization) {
+        const token = Authorization.replace('Bearer ', '')
+        //console.log('token', token)
+        const verifiedToken = verify(token, APP_SECRET) as Token
+        //console.log('tokenverified', verifiedToken && verifiedToken.userId)
+        context.userId = verifiedToken && verifiedToken.userId
+      }
+      return context
+    },
+  },
 })
 
-server.start(
+server.listen(
   {
-    subscriptions: {
-      onConnect: (connectionParams: any, request: any, context: Context) => {
-        //console.log('onConnect')
-        let Authorization
-        if (connectionParams.Authorization) {
-          //console.log('websocket', connectionParams.Authorization)
-          Authorization = connectionParams.Authorization
-        }
-        if (Authorization) {
-          const token = Authorization.replace('Bearer ', '')
-          //console.log('token', token)
-          const verifiedToken = verify(token, APP_SECRET) as Token
-          //console.log('tokenverified', verifiedToken && verifiedToken.userId)
-          context.userId = verifiedToken && verifiedToken.userId
-        }
-        return context
-      },
-    },
     cors: {
       credentials: true,
       origin: ['http://localhost:3000'],
