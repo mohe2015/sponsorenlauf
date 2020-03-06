9e82313ff29496d32b296f2bafacc859912517c0
dev.mohe <Moritz.Hedtke@t-online.de>
2020-03-06 23:03:58 +0100

    Some fixes

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,17 @@
 import { schema } from 'nexus-future'
+import { verify, Secret } from 'jsonwebtoken'
 
- schema.addToContext(req => {
-   return { userId: 4 }
- })
+function requestToUserID(req: import("http").IncomingMessage) {
+  const token = req.headers.authorization?.replace('Bearer ', '')
+  console.log("token", token)
+  if (!token) {
+    return null;
+  }
+  const verifiedToken = verify(token as string, process.env.APP_SECRET as Secret) as NexusContext
+  console.log("verifiedToken", verifiedToken)
+  return verifiedToken.userId
+}
+
+schema.addToContext(req => {
+  return { userId: requestToUserID(req) }
+})
