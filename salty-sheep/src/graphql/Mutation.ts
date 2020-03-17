db05f35b8cd0a0f6514d77899830095228a018e0
dev.mohe <Moritz.Hedtke@t-online.de>
2020-03-17 22:55:52 +0100

    More code

---
+++ salty-sheep/src/graphql/Mutation.ts
@@ -0,0 +1,72 @@
+import { schema } from 'nexus-future'
+//import { hashSync, compare } from 'bcrypt'
+//import { sign, Secret } from 'jsonwebtoken'
+
+export const Mutation = schema.mutationType({
+  definition(t) {
+    t.crud.createOneUser({
+      computedInputs: {
+        password: ({ args }) => {
+          // @ts-ignore
+          hashSync(args.data.password, 10)
+        },
+      },
+    })
+
+    t.field('login', {
+      type: 'AuthPayload',
+      args: {
+        name: schema.stringArg({ nullable: false }),
+        password: schema.stringArg({ nullable: false }),
+      },
+      resolve: async (_parent, { name, password }, context) => {
+        const user = await context.db.user.findOne({
+          where: {
+            name,
+          },
+        })
+        if (!user) {
+          throw new Error(`No user found with name: ${name}`)
+        }
+        // @ts-ignore
+        const passwordValid = await compare(password, user.password)
+        if (!passwordValid) {
+          throw new Error('Invalid password')
+        }
+        return {
+          token: sign({ userId: user.id }, process.env.APP_SECRET as Secret),
+          user,
+        }
+      },
+    })
+
+    t.field('createOneRound', {
+      type: "Round",
+      args: {
+        startNumber: schema.intArg({ nullable: false }),
+      },
+      resolve: async (parent, { startNumber }, ctx) => {
+        const round = await ctx.db.round.create({
+          data: {
+            time: 1337, // TODO
+            student: {
+              connect: {
+                startNumber: startNumber, // TODO FIXME doesn't check if it exists
+              },
+            },
+            createdBy: {
+              connect: {
+                id: ctx.userId,
+              },
+            },
+          },
+        })
+        console.log("publish rounds", round)
+        ctx.pubsub.publish('ROUNDS', round)
+        return round
+      },
+    })
+
+    t.crud.createOneStudent()
+  },
+})
