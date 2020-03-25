153af69f77d81fce8a08698827d9e676acd5cef8
dev.mohe <Moritz.Hedtke@t-online.de>
2020-03-25 17:56:29 +0100

    Temporary fix

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,16 @@
 import { verify, Secret } from 'jsonwebtoken'
-import { permissions } from './permissions';
+import { permissions } from './permissions'
 import { applyMiddleware } from 'graphql-middleware'
-import { PubSub } from 'graphql-subscriptions';
+import { PubSub } from 'graphql-subscriptions'
 import { mergeSchemas } from 'graphql-tools'
-import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt } from 'graphql';
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+} from 'graphql'
 import { ApolloServer } from 'apollo-server'
-import { schema, server, settings, log } from "nexus-future"
+import { schema, server, settings, log } from 'nexus-future'
 import { ExecutionParams } from 'subscriptions-transport-ws'
 import { printSchema } from 'graphql'
 import fs from 'fs'
@@ -17,66 +22,71 @@ interface WebSocketContext {
 }
 
 function requestToUserID(param: any) {
-  let req: import("http").IncomingMessage = param.req; // WTF?
-  let connection: ExecutionParams<WebSocketContext> = param.connection;
-  let authorization = connection?.context?.Authorization || req.headers.authorization;
+  let req: import('http').IncomingMessage = param.req // WTF?
+  let connection: ExecutionParams<WebSocketContext> = param.connection
+  let authorization =
+    connection?.context?.Authorization || req.headers.authorization
   const token = authorization?.replace('Bearer ', '')
   if (!token) {
-    return null;
+    return null
   }
-  const verifiedToken = verify(token as string, process.env.APP_SECRET as Secret) as NexusContext
+  const verifiedToken = verify(
+    token as string,
+    process.env.APP_SECRET as Secret,
+  ) as NexusContext
   return verifiedToken.userId
 }
 
-schema.addToContext(req => {
+schema.addToContext((req) => {
   return {
     userId: requestToUserID(req),
-    pubsub: pubSub
+    pubsub: pubSub,
   }
 })
 
 settings.change({
   schema: {
-    generateGraphQLSDLFile: "generated/graphql.schema",
+    //generateGraphQLSDLFile: "generated/graphql.schema",
     connections: {
       default: {
-        includeNodesField: true
-      }
+        includeNodesField: true,
+      },
     },
-  }
+  },
 })
 
 // https://github.com/apollographql/graphql-subscriptions/blob/master/src/test/asyncIteratorSubscription.ts
 
 function buildSchema(schema: GraphQLSchema) {
-  const subscriptionSchema = new GraphQLSchema ({
+  const subscriptionSchema = new GraphQLSchema({
     // you can ignore this...graphql just wants to me to have a query
-    query: new GraphQLObjectType({ name: 'RootQueryType', fields: { fooQuery: { type: GraphQLInt, resolve: source => source } } }),
+    query: new GraphQLObjectType({
+      name: 'RootQueryType',
+      fields: { fooQuery: { type: GraphQLInt, resolve: (source) => source } },
+    }),
 
     subscription: new GraphQLObjectType({
       name: 'Subscription',
       fields: {
         subscribeRounds: {
-          type: schema.getType("Round") as GraphQLObjectType,
+          type: schema.getType('Round') as GraphQLObjectType,
           // subscribe: withFilter(() => iterator, filterFn),
           subscribe: (source, args, context, info) => {
-            return context.pubsub.asyncIterator("ROUNDS")
+            return context.pubsub.asyncIterator('ROUNDS')
           },
           resolve: (source, args, context, info) => {
             console.log(source)
             return source
           },
-        }
-      }
-    })
-  });
+        },
+      },
+    }),
+  })
   schema = mergeSchemas({
-    schemas: [
-      schema, 
-      subscriptionSchema
-  ]})
+    schemas: [schema, subscriptionSchema],
+  })
   schema = applyMiddleware(schema, permissions)
-  //fs.writeFileSync("generated/schema.graphql", printSchema(schema))
+  fs.writeFileSync('generated/schema.graphql', printSchema(schema))
   return schema
 }
 
@@ -84,7 +94,7 @@ server.custom(({ schema, context, express }) => {
   schema = buildSchema(schema)
   const server = new ApolloServer({
     schema,
-    context
+    context,
   })
 
   return {
@@ -92,11 +102,11 @@ server.custom(({ schema, context, express }) => {
       await server.listen()
 
       console.log(`Apollo Server listening`, {
-        url: server.graphqlPath
+        url: server.graphqlPath,
       })
     },
     stop() {
       return server.stop()
-    }
+    },
   }
 })
