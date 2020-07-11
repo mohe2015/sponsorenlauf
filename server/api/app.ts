d8c4d7e85350c29f9dd1080d4712a36df417e4f8
dev.mohe <Moritz.Hedtke@t-online.de>
2020-07-11 15:28:38 +0200

    Hopefully some fixes

--- server/api/app.ts
+++ server/api/app.ts
@@ -1,20 +1,72 @@
 import { use } from "nexus";
 import { schema } from "nexus";
+import { server } from "nexus";
+import { log } from "nexus";
 import { settings } from "nexus";
 import { prisma } from "nexus-plugin-prisma";
+import { PrismaClient } from "nexus-plugin-prisma/client";
 import { PubSub } from "graphql-subscriptions";
+import { subscriptions } from "nexus-plugin-subscriptions";
+import { permissions } from "./permissions";
+import { verify, Secret } from "jsonwebtoken";
+
+interface BearerToken {
+  userId: string;
+}
+
+const db = new PrismaClient();
+const pubsub = new PubSub();
 
 use(
   prisma({
+    client: {
+      instance: db,
+    },
     features: {
       crud: true,
     },
   })
 );
 
-schema.addToContext(() => {
-  return {
-    pubsub: new PubSub(),
-    userId: "",
-  };
+//use(permissions());
+
+use(
+  subscriptions({
+    ws: { server: server.raw.http, path: "/graphql" }, // use server.raw.http here
+    keepAlive: 10 * 1000,
+    onConnect: (payload: Record<string, any>) => {
+      log.info("client connected");
+      return createContext(payload["authorization"]);
+    },
+    onDisconnect: () => {
+      log.info("client disconnected");
+    },
+  })
+);
+
+schema.addToContext(async (req) => {
+  return await createContext(req.headers["authorization"]!);
 });
+
+async function createContext(authorization: string) {
+  const match = /^Bearer (.*)$/.exec(authorization);
+  if (match) authorization = match[1];
+
+  if (authorization) {
+    const verifiedToken = verify(
+      authorization,
+      process.env.APP_SECRET as Secret
+    ) as BearerToken;
+    return {
+      userId: verifiedToken.userId,
+      pubsub,
+      db,
+    };
+  } else {
+    return {
+      userId: null,
+      pubsub,
+      db,
+    };
+  }
+}
--- server/api/graphql/Subscription.ts
+++ server/api/graphql/Subscription.ts
@@ -1,14 +1,21 @@
 import { schema } from "nexus";
+import { withFilter } from "graphql-subscriptions";
+import { Round, RoundWhereUniqueInput } from "nexus-plugin-prisma/client";
 
 schema.subscriptionType({
   definition(t) {
     t.field("test", {
       type: "Subscription",
-      subscribe(root, args, context, info) {
-        return context.pubsub.asyncIterator("ROUNDS");
-      },
-      resolve(root, args, context, info) {
-        return root;
+      subscribe: withFilter(
+        function (root, args, context, info) {
+          return context.pubsub.asyncIterator("ROUNDS");
+        },
+        (payload: Round, args: RoundWhereUniqueInput) => {
+          return true;
+        }
+      ),
+      resolve(payload: Round, args, context, info) {
+        return payload;
       },
     });
   },
