11b1ef0c31b6a138f2e3e6740205cf90955cc16b
dev.mohe <Moritz.Hedtke@t-online.de>
2020-07-13 23:14:47 +0200

    More UI

--- client2/src/users/User.tsx
+++ client2/src/users/User.tsx
@@ -1,12 +1,38 @@
-
 import React from "react"
 import { createFragmentContainer } from "react-relay"
 import { graphql } from "babel-plugin-relay/macro";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import ControlledTooltip from "../ControlledTooltip";
+import { IconButton, Typography, Box } from "@material-ui/core";
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function User(props: any) {
   const {id, name, role} = props.user;
   return (
-    <div>{name}</div>
+    <TableRow key={name}>
+      <TableCell component="th" scope="row">
+        {name}
+      </TableCell>
+      <TableCell align="right">{role}</TableCell>
+      <TableCell align="right">
+        <ControlledTooltip title="Löschen">
+          <IconButton>
+            <DeleteIcon />
+            <Typography variant="button" noWrap>
+              <Box component="span" display={{ xs: 'none', md: 'block' }}>
+              Löschen
+              </Box>
+            </Typography>
+          </IconButton>
+        </ControlledTooltip>
+      </TableCell>
+    </TableRow>
   );
 }
 
--- client2/src/users/UserList.tsx
+++ client2/src/users/UserList.tsx
@@ -2,6 +2,16 @@ import React from "react";
 import { createFragmentContainer } from 'react-relay';
 import { graphql } from "babel-plugin-relay/macro";
 import User from "./User";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { Container, IconButton, Typography, Box } from "@material-ui/core";
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
 
 type Props = {
   list: any;
@@ -18,9 +28,30 @@ class UserList extends React.Component<Props, State> {
 
   render() {
     return (
-      <ul>
-        {this.props.list.edges.map((user: any) => <User key={user.node.id} user={user.node} />)}
-      </ul>
+      <Container maxWidth="sm">
+        <IconButton>
+          <AddIcon />
+          <Typography variant="button" noWrap>
+            <Box component="span">
+            Nutzer erstellen
+            </Box>
+          </Typography>
+        </IconButton>
+      <TableContainer component={Paper}>
+        <Table aria-label="table of users">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Rolle</TableCell>
+              <TableCell align="right">Aktionen</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {this.props.list.edges.map((user: any) => <User key={user.node.id} user={user.node} />)}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      </Container>
     );
   }
 }
