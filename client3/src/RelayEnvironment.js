3bae48f45ab46d7a7b4a5db4b264e7da56b3ef4e
dev.mohe <Moritz.Hedtke@t-online.de>
2020-07-17 21:09:30 +0200

    Add relay environment

---
+++ client3/src/RelayEnvironment.js
@@ -0,0 +1,22 @@
+// your-app-name/src/RelayEnvironment.js
+import { Environment, Network, RecordSource, Store } from 'relay-runtime';
+
+async function fetchRelay(params, variables) {
+  const response = await fetch('http://localhost:4000/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: params.text,
+      variables,
+    }),
+  });
+
+  return await response.json();
+}
+
+export default new Environment({
+  network: Network.create(fetchRelay),
+  store: new Store(new RecordSource()),
+});
