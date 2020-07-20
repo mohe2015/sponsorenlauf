ca314024a53eb3d4cc93f218d8276c18342026e6
dev.mohe <Moritz.Hedtke@t-online.de>
2020-07-20 19:51:35 +0200

    Lower test sleep

--- server/api/app.ts
+++ server/api/app.ts
@@ -87,7 +87,7 @@ schema.addToContext(async (req: Request) => {
 // https://github.com/graphql-nexus/nexus/issues/506
 async function createContext(cookie: string | null, response: Response | null) {
   // Added for debugging
-  await new Promise((r) => setTimeout(r, 10000));
+  await new Promise((r) => setTimeout(r, 3000));
   
   if (nextCleanupCheck.getTime() < Date.now()) {
     nextCleanupCheck = new Date();
