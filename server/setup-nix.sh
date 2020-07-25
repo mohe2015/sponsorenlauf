3fb46810ac58bc84beb867804d3dc5bd51c2276b
mohe2015 <Moritz.Hedtke@t-online.de>
2020-07-25 17:56:00 +0200

    Important fixes

--- server/setup-nix.sh
+++ server/setup-nix.sh
@@ -4,11 +4,16 @@ git clone https://github.com/prisma/prisma-engines/
 cd prisma-engines
 source .envrc # idk if needed
 cargo build --release
-exit # nix-shell
-cp target/release/introspection-engine ../node_modules/@prisma/cli/introspection-engine-linux-nixos
-cp target/release/migration-engine ../node_modules/@prisma/cli/migration-engine-linux-nixos
-cp target/release/query-engine ../node_modules/@prisma/cli/query-engine-linux-nixos
-cp target/release/prisma-fmt ../node_modules/@prisma/cli/prisma-fmt-linux-nixos
 cd ..
+
+cp prisma-engines/target/release/introspection-engine node_modules/@prisma/cli/introspection-engine-linux-nixos
+cp prisma-engines/target/release/migration-engine node_modules/@prisma/cli/migration-engine-linux-nixos
+cp prisma-engines/target/release/query-engine node_modules/@prisma/cli/query-engine-linux-nixos
+cp prisma-engines/target/release/prisma-fmt node_modules/@prisma/cli/prisma-fmt-linux-nixos
+
+cp prisma-engines/target/release/introspection-engine node_modules/@prisma/sdk/introspection-engine-linux-nixos
+cp prisma-engines/target/release/migration-engine node_modules/@prisma/sdk/migration-engine-linux-nixos
+cp prisma-engines/target/release/query-engine node_modules/@prisma/sdk/query-engine-linux-nixos
+cp prisma-engines/target/release/prisma-fmt node_modules/@prisma/sdk/prisma-fmt-linux-nixos
+
 yarn prisma migrate save --experimental
-# ...
