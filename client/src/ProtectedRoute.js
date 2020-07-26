de3669ad63c5da24e30b1eceea1d634959c1997c
mohe2015 <Moritz.Hedtke@t-online.de>
2020-07-26 22:53:00 +0200

    Jo

--- client/src/ProtectedRoute.js
+++ client/src/ProtectedRoute.js
@@ -2,7 +2,7 @@ import React, {Element} from 'react';
 import { Switch, Route, Navigate } from 'react-router-dom';
 
 export const ProtectedRoute = ({ children, ...rest }) => {
-    /// fail its httponly
+    // TODO FIXME this only works when a rerender happens on navigation
     if (document.cookie.split('; ').some(row => row.startsWith('logged-in='))) {
         console.log("cookie there")
         return <Route {...rest}>
