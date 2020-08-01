f8053a7e464bbffc299ab81ceb4a1586fbb33fda
mohe2015 <Moritz.Hedtke@t-online.de>
2020-08-01 17:01:08 +0200

    Test updates

--- server/setup-nix.sh
+++ server/setup-nix.sh
@@ -1,5 +1,8 @@
-yarn # install all packages, so the node_modules folder is available
-nix-shell
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p pkg-config zlib openssl nodejs
+
+#yarn # install all packages, so the node_modules folder is available
+
 git clone https://github.com/prisma/prisma-engines/
 cd prisma-engines
 source .envrc # idk if needed
@@ -15,5 +18,3 @@ cp prisma-engines/target/release/introspection-engine node_modules/@prisma/sdk/i
 cp prisma-engines/target/release/migration-engine node_modules/@prisma/sdk/migration-engine-linux-nixos
 cp prisma-engines/target/release/query-engine node_modules/@prisma/sdk/query-engine-linux-nixos
 cp prisma-engines/target/release/prisma-fmt node_modules/@prisma/sdk/prisma-fmt-linux-nixos
-
-yarn prisma migrate save --experimental
