66f4f98b7419533dab82057f27f283fb4f232921
mohe2015 <Moritz.Hedtke@t-online.de>
2020-08-18 18:15:14 +0200

    bruh

--- server/api/graphql/Mutation.ts
+++ server/api/graphql/Mutation.ts
@@ -224,16 +224,18 @@ schema.mutationType({
           let startNumber = args.data.student.connect?.startNumber;
           let thecuid = cuid();
           let createdById = context.user?.id;
-          let roundWithRunner = await context.db.$queryRaw<RoundWithRunner>`
-BEGIN;
-INSERT INTO Round (id, studentId, createdById) VALUES (${thecuid}, (SELECT id FROM Runner WHERE startNumber = ${startNumber}), ${createdById});
-UPDATE Runner SET roundCount = roundCount + 1 WHERE id = (SELECT id FROM Runner WHERE startNumber = ${startNumber});
-SELECT Round.id AS `Round.id`, Round.studentId AS `Round.studentId`, Round.createdById AS `Round.createdById`, Round.time AS `Round.time`, Runner.id AS `Runner.id`, Runner.startNumber AS `Runner.startNumber`, Runner.name AS `Runner.name`, Runner.clazz AS `Runner.clazz`, Runner.grade as `Runner.grade`, Runner.roundCount AS `Runner.roundCount` FROM Round, Runner WHERE Runner.id = (SELECT id FROM Runner WHERE startNumber = ${startNumber}) AND Round.id = ${thecuid};
-COMMIT;
-`;
 
-          console.log(roundWithRunner.time)
-          console.log(roundWithRunner.student.name)
+          // TODO FIXME this is supposed to be an transaction but there is no support in prisma and I think none of these should fail
+          await context.db.$executeRaw`INSERT INTO Round (id, studentId, createdById) VALUES (${thecuid}, (SELECT id FROM Runner WHERE startNumber = ${startNumber}), ${createdById});`
+          await context.db.$executeRaw`UPDATE Runner SET roundCount = roundCount + 1 WHERE id = (SELECT id FROM Runner WHERE startNumber = ${startNumber});`
+          let result = await context.db.$queryRaw<object[]>`SELECT Round.id AS "Round.id", Round.studentId AS "Round.studentId", Round.createdById AS "Round.createdById", Round.time AS "Round.time", Runner.id AS "Runner.id", Runner.startNumber AS "Runner.startNumber", Runner.name AS "Runner.name", Runner.clazz AS "Runner.clazz", Runner.grade as "Runner.grade", Runner.roundCount AS "Runner.roundCount" FROM Round, Runner WHERE Runner.id = (SELECT id FROM Runner WHERE startNumber = ${startNumber}) AND Round.id = ${thecuid};`
+      
+          let roundWithRunner = unflatten(result[0], {
+            object: false,
+          }) as RoundWithRunner
+          //unflattened.time = new Date(unflattened.time)
+
+          console.log(roundWithRunner)
 
           let output = {
             __typename: "CreateRoundMutationOutput",
