c8adf3093b5d3f1c90eb82e7d856212fe9b0b264
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-08-27 21:25:10 +0200

    Update packages

--- server/flake.nix
+++ server/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "my project description";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:Pamplemousse/nixpkgs/prisma";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
